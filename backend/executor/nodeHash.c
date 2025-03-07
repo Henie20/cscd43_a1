1b1a95124e0fd63d90325de9cbe2b79a13991a19
Swetha Poneasan <poneasan@mathlab.utsc.utoronto.ca>
2025-03-07 14:54:47 -0500

    hash functions

--- backend/executor/nodeHash.c
+++ backend/executor/nodeHash.c
@@ -422,6 +422,35 @@ ExecEndHash(HashState *node)
 }
 
 
+
+// BEGIN NEWCODE
+int hash_function1(int value, int table_size) 
+{
+	int hashed_value;
+	float c = 0.21;
+	hashed_value = floor(table_size * (value * (int) c % 1));
+	return hashed_value;
+}
+
+int hash_function2(int value, int table_size)
+{
+	return value % table_size;
+}
+
+int hash_function3(int value, int table_size)
+{
+	int hashed_value = value * (value + 3) % table_size;
+}
+
+int hash_function4(int value, int table_size)
+{
+	int hashed_value;
+	float c = 0.77;
+	hashed_value = floor(table_size * (value * (int) c % 1));
+	return hashed_value;
+}
+// END NEWCODE
+
 /* ----------------------------------------------------------------
  *		ExecHashTableCreate
  *
