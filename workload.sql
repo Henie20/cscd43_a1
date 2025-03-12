7bcf723539db55f784f19a7510f1c5ec0c325cd6
Swetha <74365171+swepon@users.noreply.github.com>
2025-03-12 00:44:32 -0400

    Update workload.sql

--- workload.sql
+++ workload.sql
@@ -1,3 +1,73 @@
+-- Varying Bit Array
+EXPLAIN (ANALYZE) SELECT COUNT(*)
+FROM testr, tests
+WHERE testr.id = tests.id AND
+testr.id >= 1000 AND
+testr.id <= 2000 AND
+tests.id >= 1500 AND
+tests.id <= 2500;
+-- Vary size of inner relation
+EXPLAIN (ANALYZE) SELECT COUNT(*)
+FROM testr, tests
+WHERE testr.id = tests.id AND
+testr.id > 10 AND
+tests.id > 50 AND 
+tests.id <= 100;
+
+EXPLAIN (ANALYZE) SELECT COUNT(*)
+FROM testr, tests
+WHERE testr.id = tests.id AND
+testr.id > 10 AND
+tests.id > 1000 AND tests.id <= 2000;
+
+EXPLAIN (ANALYZE) SELECT COUNT(*)
+FROM testr, tests
+WHERE testr.id = tests.id AND
+testr.id > 10 AND
+tests.id > 1000 AND tests.id <= 3000;
+
+EXPLAIN (ANALYZE) SELECT COUNT(*)
+FROM testr, tests
+WHERE testr.id = tests.id AND
+testr.id > 10 AND
+tests.id > 5000;
+
+EXPLAIN (ANALYZE) SELECT COUNT(*)
+FROM testr, tests
+WHERE testr.id = tests.id AND
+testr.id > 10 AND
+tests.id <= 9000;
+
+-- Vary size of outer relation
+EXPLAIN (ANALYZE) SELECT COUNT(*)
+FROM testr, tests
+WHERE testr.id = tests.id AND
+testr.id > 50 AND testr.id <= 100 AND
+tests.id < 10;
+
+EXPLAIN (ANALYZE) SELECT COUNT(*)
+FROM testr, tests
+WHERE testr.id = tests.id AND
+testr.id > 1000 AND testr.id <= 2000 AND
+tests.id < 10;
+
+EXPLAIN (ANALYZE) SELECT COUNT(*)
+FROM testr, tests
+WHERE testr.id = tests.id AND
+testr.id > 1000 AND testr.id <= 3000 AND
+tests.id < 10;
+
+EXPLAIN (ANALYZE) SELECT COUNT(*)
+FROM testr, tests
+WHERE testr.id = tests.id AND
+testr.id > 5000 AND
+tests.id < 10;
+
+EXPLAIN (ANALYZE) SELECT COUNT(*)
+FROM testr, tests
+testr.id <= 9000 AND
+tests.id < 10;
+
 EXPLAIN (ANALYZE) 
 SELECT testr.id, tests.id 
 FROM testr 
