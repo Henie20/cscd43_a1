0bb8c0b1655bc60b86aac1c57cfaeb1c8b0d5a1a
Swetha Poneasan <poneasan@mathlab.utsc.utoronto.ca>
2025-03-15 16:57:43 -0400

    update queries

--- workload.sql
+++ workload.sql
@@ -9,31 +9,26 @@ S.id < 3000;
 EXPLAIN (ANALYZE) SELECT COUNT(*)
 FROM R, S
 WHERE R.id = S.id AND
-R.id > 50 AND R.id <= 100 AND
-S.id < 10;
+R.id >= 6000 AND 
+S.id < 3000;
 
 EXPLAIN (ANALYZE) SELECT COUNT(*)
 FROM R, S
 WHERE R.id = S.id AND
-R.id > 1000 AND R.id <= 2000 AND
-S.id < 10;
+R.id >= 5000 AND 
+S.id < 3000;
 
 EXPLAIN (ANALYZE) SELECT COUNT(*)
 FROM R, S
 WHERE R.id = S.id AND
-R.id > 1000 AND R.id <= 3000 AND
-S.id < 10;
+R.id >= 4000 AND 
+S.id < 3000;
 
 EXPLAIN (ANALYZE) SELECT COUNT(*)
 FROM R, S
 WHERE R.id = S.id AND
-R.id > 5000 AND
-S.id < 10;
-
-EXPLAIN (ANALYZE) SELECT COUNT(*)
-FROM R, S
-WHERE R.id <= 9000 AND
-S.id < 10;
+R.id >= 3000 AND 
+S.id < 3000;
 
 -- Vary size of inner relation
 EXPLAIN (ANALYZE) SELECT COUNT(*) 
